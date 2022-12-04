238ea8ba1e4f23f2e724ed68ee4f6d18c579e274
MuuStranger <113274772+MuuStranger@users.noreply.github.com>
2022-12-04 09:55:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,7 +59,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !!,Dᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴀɴʏ ᴍᴏᴠɪᴇ. Pʟᴇᴀsᴇ Rᴇǫᴜᴇsᴛ Oɴ @MM_Archives</b>")
+    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !!,Dᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴀɴʏ ᴍᴏᴠɪᴇ. Pʟᴇᴀsᴇ Rᴇǫᴜᴇsᴛ Oɴ @MCC_OTT</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
@@ -446,8 +446,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         reply_markup=InlineKeyboardMarkup(
                               [
                            [
-                            InlineKeyboardButton('⚒ 𝗠𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟', url=f"https://t.me/Movie_Meadia"),
-                            InlineKeyboardButton('📌 𝗠𝗬 𝗚𝗥𝗢𝗨𝗣', url=f"https://t.me/MM_Archives")
+                            InlineKeyboardButton('⚒ 𝗠𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟', url=f"https://t.me/Movie_Collections_Channel"),
+                            InlineKeyboardButton('📌 𝗠𝗬 𝗚𝗥𝗢𝗨𝗣', url=f"https://t.me/MCC_OTT")
                           ]
                         ]
                     )
